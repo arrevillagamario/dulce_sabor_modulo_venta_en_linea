d032f4ef3f4ab1925e193269581b4b87145b3525
arrevillagamario <marrevillaga03@gmail.com>
2024-04-15 17:43:21 -0600

    Agregar archivos de proyecto.

---
+++ dulce_sabor_modulo_venta_en_linea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dulce_sabor_modulo_venta_en_linea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
