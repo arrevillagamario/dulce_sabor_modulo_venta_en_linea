3bfae89ca060615cb907b53aeadc1cc5d1b68a2d
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2024-04-16 00:08:06 -0600

    Reorganización de Enrrutamiento

---
+++ dulce_sabor_modulo_venta_en_linea/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dulce_sabor_modulo_venta_en_linea.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Crear() 
+        {
+            return View();
+        }
+    }
+}
